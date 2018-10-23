33a9570b52bf289024e5b65b5bfdc2bc2e820bfa
liuleigang <liu_lg@neusoft.com>
2018-10-23 10:13:12 +0800

    Video Player Example

---
+++ VideoPlayer/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import RootScreen from "./src/screen/RootScreen";
+
+type Props = {};
+export default class App extends Component<Props> {
+    render() {
+        return (
+            <RootScreen/>
+        );
+    }
+}
